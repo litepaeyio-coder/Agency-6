a63484f18f04913956db26874e7287a9e29c7209
litepaeyio-coder <litepaeyio@gmail.com>
2025-09-11 00:56:49 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: { inter: ['Inter', 'sans-serif'] },
+      colors: { primary: '#2563eb' }
+    }
+  },
+  plugins: []
+};
